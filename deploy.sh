0525094e447c647fb6c8962feb8764b67a02faed
Farhafs1 <csc190108@students.umyu.edu.ng>
2026-05-27 16:48:51 +0100

    Explicitly add deploy script to repository

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 1. Run the database migrations safely at boot
+echo "🚀 Running database migrations..."
+php artisan migrate --force
+
+# 2. Hand control back over to the image's original entrypoint 
+echo "🎬 Starting Nginx and PHP-FPM..."
+exec /entrypoint /init
